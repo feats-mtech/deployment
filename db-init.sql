9064748c79ff43f50a02b347c0287bb74d53f405
Desmond Tan <desmondtzh@hotmail.com>
2024-09-19 22:03:08 +0800

    db init sql

---
+++ db-init.sql
@@ -0,0 +1,10 @@
+
+create TABLE IF NOT EXISTS user_account(
+	user_id varchar not null,
+	user_name varchar not null,
+	user_password varchar not null,
+	user_email varchar not null,
+	user_status INTEGER
+	
+	);
+	
