223d669f2231404fe6702a3c2f5fed6692ba4f38
Pinardy <pinardy.yang@gmail.com>
2025-02-28 16:39:03 +0800

    feat: add google login to digitalocean setup (#35)
    * feat: add google variables for terraform setup
    
    * chore: remove unused k8 files
    
    * feat: api gw endpoints for oauth2
    
    * feat: add google vars to backend k8 files

--- iac/prod/backend.tf
+++ iac/prod/backend.tf
@@ -12,5 +12,7 @@ resource "kubernetes_secret" "feats_backend_secret" {
     POSTGRES_K8_PROD_PASSWORD  = digitalocean_database_user.db_user.password
     POSTGRES_K8_PROD_PORT      = digitalocean_database_cluster.postgresql.port
     POSTGRES_K8_PROD_IP        = digitalocean_database_cluster.postgresql.host
+    GOOGLE_OAUTH_CLIENT_ID     = var.google_client_id
+    GOOGLE_OAUTH_CLIENT_SECRET = var.google_client_secret
   }
 }
